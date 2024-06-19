30b3bf2e0c2060965b768591efb7c2733fa73b55
DRaymond <danimator1@gmail.com>
2024-06-19 12:05:38 -0400

    update

--- react_axios_foodE/src/components/Surprise.jsx
+++ react_axios_foodE/src/components/Surprise.jsx
@@ -1,5 +1,27 @@
-export default function Surprise () {
-    return (
-        <h1>Surprise</h1>
-    )
+import { useEffect, useState } from 'react'
+import {  Link } from 'react-router-dom'
+import axios from 'axios'
+
+const RandomDetails = () => {
+  const [random, setRandom] = useState([])
+
+  useEffect(() => {
+    const getRandom = async () => {
+      const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/random.php`)
+      setRandom(response.data.meals[0])
+    }
+    getRandom()
+  }, [])
+
+  
+  return (
+    <div className="random">
+      <h2>{random.strMeal}</h2>
+      <Link to={`/dishes/${random.idMeal}`}>
+      <img src= "https://www.themealdb.com/images/media/meals/vdwloy1713225718.jpg" alt={random.strMeal} className="radomImage"/>
+      </Link>
+    </div>
+  )
 }
+
+export default RandomDetails
