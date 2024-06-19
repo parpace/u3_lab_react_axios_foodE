d40818515023ab2c6761489d94149b68f671cada
parpace <parpace@gmail.com>
2024-06-19 10:02:53 -0500

    nothing

--- react_axios_foodE/src/components/Countries.jsx
+++ react_axios_foodE/src/components/Countries.jsx
@@ -1,6 +1,7 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
 import Carousel from 'react-bootstrap/Carousel'
+import { Link } from "react-router-dom"
 
 export default function Travel() {
     const [countries, setCountries] = useState([])
